b302df826d0f5fa06dfd149b3ae5582ef5df8bfb
Gandrakoti-Dinesh <165664222+Gandrakoti-Dinesh@users.noreply.github.com>
2026-03-12 14:36:04 +0530

    Add Employee class with constructor and properties

---
+++ Proxy Pattern/Employee.cs
@@ -0,0 +1,13 @@
+namespace SecureDocs //Do NOT change the Namespace Name
+{
+    public class Employee //Do NOT change the Class Name
+    {
+        public string documentId;
+        public string employeeName;
+       // Implement your code here
+       public Employee(string documentId,string employeeName){
+           this.documentId=documentId;
+           this.employeeName=employeeName;
+       }
+    }
+}
