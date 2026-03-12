7925a7f2cfb7f5ee223ce10392dc8d753681423f
Gandrakoti-Dinesh <165664222+Gandrakoti-Dinesh@users.noreply.github.com>
2026-03-12 14:29:41 +0530

    Create Gadget class in GadgetHub namespace

---
+++ Factory Pattern/Gadget.cs
@@ -0,0 +1,8 @@
+namespace GadgetHub //Do NOT change the Namespace Name
+{
+    public class Gadget //Do NOT change the Class Name
+    {
+        // Implement your code here
+        public string gadgetName;
+    }
+}
